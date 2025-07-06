5272f26924352ee8ffb594c9c9b69ca99affaec8
Error404 <107164653+alanwang233233@users.noreply.github.com>
2025-07-06 21:17:02 +0800

    Update core.php

--- core.php
+++ core.php
@@ -2,10 +2,6 @@
 
 //下列为设置项
 $frameurl = "http://api.rel.lxyddice.top:55520/wang/latest/";
-
-
-
-
 include "function/function.php";
 stoperror();
 $fckey = "ujdksghfuyegfuysd114514";
@@ -13,30 +9,23 @@ $postdata = file_get_contents("php://input");
 $zhiling = NULL;
 //获得用户消息，已弃用
 $msg1 = json_decode($postdata, true)['text']['content'];
-$usermsg = trim($msg1); // 去除首尾空格
-// 判断是否存在PHP代码
+$usermsg = trim($msg1);
 if (strpos($usermsg, '<?php') !== false || strpos($usermsg, '?>') !== false) {
-    // 去除PHP代码
+    //去除PHP代码
     $usermsg = preg_replace('/<\?php(.*?)\?>/s', '', $usermsg);
 }
-// 判断是否存在HTML标签
 if (strpos($usermsg, '<') !== false || strpos($usermsg, '>') !== false) {
     // 去除HTML标签
     $usermsg = strip_tags($usermsg);
 }
 $usermsg = str_replace(['$','disabled','(',')'], '', $usermsg);
 //$usermsg = str_replace([ '\\', '/'], '', $usermsg);
-// 使用explode函数将用户消息按空格分割数组
 $usermsg_parts = explode(" ", $usermsg);
-// 获取指令部分（第一个元素）
 $zhiling = $usermsg_parts[0];
-// 获取参数部分（从第二个元素开始）
 $canshus = array_slice($usermsg_parts, 1);
 $var = $canshus;
-//访问数组的第一个元素（索引为 0）
 $canshu = $var[0];
 $url="$frameurl/plugin/$zhiling/?fckey=$fckey";
-// 构造POST请求数据
 $data = array(
     'data' => $postdata,
     'cs' => $canshu,
@@ -50,9 +39,7 @@ $options = array(
     )
 );
 $context = stream_context_create($options);
-// 发送POST请求
 $response = file_get_contents($url, false, $context);
-// 输出服务器的响应
 if ($response === false) {
     $response = "语法错误,帮助：help,你输入了：$usermsg,地址：$url";
 }
